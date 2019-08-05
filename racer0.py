db01b281ae96a2b85dfd8f60167cfbc5213e4288
Paul Standley <paulstandley1972@gmail.com>
2019-08-05 21:15:09 +0100

    race 0

---
+++ racer0.py
@@ -0,0 +1,27 @@
+def reward_function(params):
+    '''
+    Example of rewarding the agent to follow center line
+    '''
+    
+    # Read input parameters
+    track_width = params['track_width']
+    distance_from_center = params['distance_from_center']
+    all_wheels_on_track = params['all_wheels_on_track']
+    
+    # Calculate 3 markers that are at varying distances away from the center line
+    marker_1 = 0.1 * track_width
+    marker_2 = 0.25 * track_width
+    marker_3 = 0.5 * track_width
+    
+    # Give a high reward if no wheels go off the track and
+    # Give higher reward if the car is closer to center line and vice versa
+    if all_wheels_on_track and distance_from_center <= marker_1:
+        reward = 1.0
+    elif all_wheels_on_track and distance_from_center <= marker_2:
+        reward = 0.5
+    elif all_wheels_on_track and distance_from_center <= marker_3:
+        reward = 0.1
+    else:
+        reward = 1e-3  # likely crashed/ close to off track
+    
+    return float(reward)
