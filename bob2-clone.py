5e36e3779f450aba0dd335f70ccf67757bdd00b5
Paul Standley <paulstandley1972@gmail.com>
2019-09-03 20:52:17 +0100

    bob 2 clone

--- bob2-clone.py
+++ bob2-clone.py
@@ -13,8 +13,8 @@ def reward_function(params):
     
     # Calculate 3 markers that are at varying distances away from the center line
     marker_1 = 0.1 * track_width
-    marker_2 = 0.25 * track_width
-    marker_3 = 0.5 * track_width
+    marker_2 = 0.30 * track_width
+    marker_3 = 0.6 * track_width
     
     # Give a high reward if no wheels go off the track and
     # Give higher reward if the car is closer to center line and vice versa
@@ -23,18 +23,18 @@ def reward_function(params):
         if distance_from_center <= marker_1:
             reward = 1.0
         elif distance_from_center <= marker_2:
-            reward = 0.5
-        elif distance_from_center <= marker_3:
             reward = 0.1
+        elif distance_from_center <= marker_3:
+            reward = 0.01
     else:
         reward = 1e-3  # did crashed/ wheels off track
         
     # Steering penality threshold, change the number based on your action space setting
-    ABS_STEERING_THRESHOLD = 15
+    ABS_STEERING_THRESHOLD = 20
 
     # Penalize reward if the agent is steering too much
     if steering > ABS_STEERING_THRESHOLD:
-        reward *= 0.8
+        reward *= 0.6
 
     return float(reward)
   
